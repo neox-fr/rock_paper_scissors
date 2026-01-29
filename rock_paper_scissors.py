382d367c5d61e09d86d9407f3a26ba2317d4d6e5
neox <mirjalbajra@gmail.com>
2026-01-29 22:01:38 +0545

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,37 @@
+import random
+
+comp=["rock", "paper", "scissors"]
+def get_choices(comp):
+    player_choice=input("enter (rock, paper, scissors):")
+    computer_choice = random.choice(comp)
+    choices={"player":player_choice, "computer":computer_choice}
+    return choices
+
+def check_win(player, computer, comp):
+    print("you chose " + player + ", computer chose " + computer)
+    #print(f"you chose {player}, computer chose {computer}")
+    if player is not comp:
+        return "Not valid"
+    elif player==computer:
+        return "draw"
+    elif player=="rock":
+        if computer=="scissors":
+            return "rock smashes scissors so u win"
+        else:
+            return "paper gets rock so u lose"
+        
+    elif player=="paper":
+        if computer=="scissors":
+            return "scissors cuts paper so u lose"
+        else:
+            return "paper gets rock so u lose"
+        
+    elif player=="scissors":
+        if computer=="paper":
+            return "scissors cuts paper so u win"
+        else:
+            return " rock destroy scissors so u lose"
+
+choices = get_choices(comp)
+result = check_win(choices["player"], choices["computer"], comp)
+print(result)
